e4174f3a81b657db24ebadf0cf8df564002df77a
Leejihun <ljh09060@gmail.com>
2026-01-23 10:30:30 +0900

    fix: gpg 서명키 만료로 인한 문제 해결

--- scripts/setup/install/mysql.sh
+++ scripts/setup/install/mysql.sh
@@ -6,10 +6,10 @@ if command -v mysql &> /dev/null && mysql --version 2>&1 | grep -q "${MYSQL_VERS
 fi
 
 echo "=== MySQL APT 저장소 추가 ==="
-wget -q https://dev.mysql.com/get/mysql-apt-config_0.8.33-1_all.deb
+wget -q https://dev.mysql.com/get/mysql-apt-config_0.8.36-1_all.deb
 echo "mysql-apt-config mysql-apt-config/select-server select mysql-8.0" | sudo debconf-set-selections
-sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.33-1_all.deb
-rm -f mysql-apt-config_0.8.33-1_all.deb
+sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.36-1_all.deb
+rm -f mysql-apt-config_0.8.36-1_all.deb
 sudo apt update
 
 echo "=== MySQL ${MYSQL_VERSION} 설치 ==="
