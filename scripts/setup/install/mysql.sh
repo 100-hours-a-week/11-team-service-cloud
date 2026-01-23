b8cd766ff49bc88b9b2915636e0235b08d9156be
Leejihun <ljh09060@gmail.com>
2026-01-23 10:46:29 +0900

    fix: mysql 8.0.44 설치 안되는 문제 해결

--- scripts/setup/install/mysql.sh
+++ scripts/setup/install/mysql.sh
@@ -6,11 +6,10 @@ if command -v mysql &> /dev/null && mysql --version 2>&1 | grep -q "${MYSQL_VERS
 fi
 
 echo "=== MySQL APT 저장소 추가 ==="
-LATEST_MYSQL_APT_CONFIG="mysql-apt-config_latest_all.deb"
-wget -q https://dev.mysql.com/get/${LATEST_MYSQL_APT_CONFIG}
+wget -q https://dev.mysql.com/get/mysql-apt-config_0.8.36-1_all.deb
 echo "mysql-apt-config mysql-apt-config/select-server select mysql-8.0" | sudo debconf-set-selections
-sudo DEBIAN_FRONTEND=noninteractive dpkg -i ${LATEST_MYSQL_APT_CONFIG}
-rm -f ${LATEST_MYSQL_APT_CONFIG}
+sudo DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.36-1_all.deb
+rm -f mysql-apt-config_0.8.36-1_all.deb
 sudo apt update
 
 echo "=== MySQL ${MYSQL_VERSION} 설치 ==="
