5066cff4ca7908fa3e55bd6f7a40e24560d697b2
Lee Jihun <ljh09060@gmail.com>
2026-01-27 23:45:37 +0900

    feat: aws 설치 및 파라미터 스토어에서 .env 파일 적용

--- terraform/main.tf
+++ terraform/main.tf
@@ -200,6 +200,7 @@ resource "aws_instance" "bigbang_instance" {
   key_name               = "kateboo-11team.pem"
   vpc_security_group_ids = [aws_security_group.ssh.id]
   ipv6_address_count     = 1
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   root_block_device {
     volume_size           = 20
@@ -213,18 +214,35 @@ resource "aws_instance" "bigbang_instance" {
 
     # 패키지 설치
     apt-get update
-    apt-get install -y make wget
+    apt-get install -y make wget unzip
+
+    # AWS CLI v2 설치
+    cd /tmp
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    ./aws/install
+    rm -rf aws awscliv2.zip
 
     # GitHub develop 브랜치 다운로드
     cd /home/ubuntu
     wget https://github.com/100-hours-a-week/11-team-service-cloud/archive/refs/heads/develop.tar.gz
     tar -xzf develop.tar.gz
-    cd 11-team-service-cloud-develop
+
+    # Parameter Store에서 .env 파일 가져오기
+    echo "=== Fetching .env from Parameter Store ==="
+    /usr/local/bin/aws ssm get-parameter \
+      --name "/bigbang/dot-env" \
+      --with-decryption \
+      --region ap-northeast-2 \
+      --query 'Parameter.Value' \
+      --output text > /tmp/temp-env
+    mv /tmp/temp-env /home/ubuntu/11-team-service-cloud-develop/.env
 
     # 소유권 변경
     chown -R ubuntu:ubuntu /home/ubuntu/11-team-service-cloud-develop
 
     # 전체 환경 세팅
+    cd /home/ubuntu/11-team-service-cloud-develop
     make setup-all
   EOF
 
