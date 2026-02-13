598f71f2e060ef60f5140488fdda49d2c8b5a20e
Jimin Kim <0305jimin@gmail.com>
2026-02-13 14:27:10 +0900

    feat: attaching ecr readonly role to instance

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -33,6 +33,12 @@ resource "aws_iam_role_policy_attachment" "ssm_core" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+# Allow instances to authenticate to ECR and pull images
+resource "aws_iam_role_policy_attachment" "ecr_readonly" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 # Parameter Store read (scoped by prefix)
 resource "aws_iam_role_policy" "ssm_read_policy" {
   name = "ssm-read-policy"
